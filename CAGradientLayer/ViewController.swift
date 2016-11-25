57fcfcf0ec23ab0917b8fe95a3de843d2aee6152
sqluo <sqluo@sqluodeMac-mini.local>
2016-11-25 09:53:07 +0800

    Initial Commit

---
+++ CAGradientLayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CAGradientLayer
+//
+//  Created by sqluo on 2016/11/25.
+//  Copyright © 2016年 sqluo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
